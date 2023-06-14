f6fae313b2d0e12254f2af5e874d346a211bcbcc
GhosTHaise <Ghostrex2@gmail.com>
2023-06-14 20:06:49 +0300

    Create add tailwind config

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-      Hero
+    <div className='hero'>
+      <div className='flex-1 pt-36 padding-x'>
+          <h1 className='hero__title'>
+
+          </h1>
+      </div>
     </div>
   )
 }
