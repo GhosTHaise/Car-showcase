8356394ecc3cdb673b604143566e456df8f9be89
GhosTHaise <Ghostrex2@gmail.com>
2023-06-18 19:54:00 +0300

    update state data by combobox

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -26,7 +26,10 @@ const SearchManufacturer = ({
 
   return (
     <div className='search-manufacturer'>
-        <Combobox>
+        <Combobox
+            value={manufacturer}
+            onChange={setManufacturer}
+        >
             <div className='relative w-full'>
                 <Combobox.Button
                     className="absolute top-[14px]"
