9637378d755a19cbfbf018e79a72c9906a107fb3
GhosTHaise <Ghostrex2@gmail.com>
2023-06-24 20:17:30 +0300

    Create search button

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,13 +3,29 @@
 import { useState } from "react"
 
 import { SearchManufacturer } from "."
+import Image from "next/image";
 
+const SearchButton = ({ otherClasses }: {otherClasses? : string }  ) => (
+   <button 
+    type="submit"
+    className={`-ml-3 z-10 ${otherClasses}`}
+   >
+      <Image 
+        src="/magnifying-glass.svg"
+        alt="magnifying glass"
+        width={40}
+        height={40}
+        className="object-contain"
+      />
+   </button>
+);
 const SearchBar = () => {
 
-  const [manufacturer, setManufacturer] = useState();
+  const [manufacturer, setManufacturer] = useState('');
   const handleSearch = () => {
 
   }
+
 
   return (
     <form
@@ -21,6 +37,8 @@ const SearchBar = () => {
             manufacturer={manufacturer}
             setManufacturer={setManufacturer}
           />
+
+          <SearchButton otherClasses="sm:hidden" />
       </div>
     </form>
   )
