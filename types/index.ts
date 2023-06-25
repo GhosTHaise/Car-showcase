b552376cbeabf4d9c0e7ea522e19206fd78756f7
GhosTHaise <Ghostrex2@gmail.com>
2023-06-25 12:32:26 +0300

    Build CustomFilter Props

--- types/index.ts
+++ types/index.ts
@@ -43,3 +43,11 @@ export interface FilterProps{
     limit : number;
     model : string;
 }
+export interface OptionProps{
+    title : string;
+    value : string;
+}
+export  interface CustomFilterProps{
+    title : string;
+    option : OptionProps[]
+}
